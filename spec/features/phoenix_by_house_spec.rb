ee4eb23c5519a2613b03350a873cc1948bf2bbd5
Lito White <leland.white@gmail.com>
2020-09-01 09:27:55 -0600

    Initial test written

---
+++ spec/features/phoenix_by_house_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe 'As a user' do
+  it 'I can find members of the Order of the Phoenix by house, including each members name, role (if applicable), house, and Patronus (if applicable)' do
+    visit '/'
+    select "Gryffindor", from: :house
+    click_on "Search for Members"
+    expect(current_path).to eq(search_path)
+    expect(page).to have_css(".member", count: 21)
+
+    within(first(".member")) do
+      expect(page).to have_css(".name")
+      expect(page).to have_css(".role")
+      expect(page).to have_css(".house")
+      expect(page).to have_css(".patronus")
+    end
+  end
+end
